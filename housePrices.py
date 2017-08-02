746be895b277d508ee7d7d32d4e43572fb2b4aac
cecilartavion <30665264+cecilartavion@users.noreply.github.com>
2017-08-02 14:53:24 -0400

    Added Pocessed data
    There is still an error with nan. Can you fix it, Andrew?

--- housePrices.py
+++ housePrices.py
@@ -14,6 +14,8 @@ import random
 import numpy as np
 import pandas as pd
 from sklearn import datasets, svm, cross_validation, tree, preprocessing, metrics
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
 from sklearn.neural_network import MLPClassifier
 import sklearn.ensemble as ske
 from scipy import stats
@@ -33,6 +35,77 @@ class_alley_grouping = house_train_df.groupby(['Alley']).mean()
 print(str(class_alley_grouping))
 class_alley_grouping['SalePrice'].plot.bar()
 plt.show()
+
+def preprocess_house_df(df):
+    processed_df = df.copy()
+    le = preprocessing.LabelEncoder()
+    #replace sex and embarked with integers
+    processed_df.MSZoning = le.fit_transform(processed_df.MSZoning)
+    processed_df.Street = le.fit_transform(processed_df.Street)
+    processed_df.Alley = [str(x) for x in processed_df.Alley]
+    processed_df.Alley = le.fit_transform(processed_df.Alley)
+    processed_df.LotShape= le.fit_transform(processed_df.LotShape)
+    processed_df.LandContour= le.fit_transform(processed_df.LandContour)
+    processed_df.Utilities= le.fit_transform(processed_df.Utilities)
+    processed_df.LotConfig= le.fit_transform(processed_df.LotConfig)
+    processed_df.LandSlope= le.fit_transform(processed_df.LandSlope)
+    processed_df.Neighborhood= le.fit_transform(processed_df.Neighborhood)
+    processed_df.Condition1= le.fit_transform(processed_df.Condition1)
+    processed_df.Condition2= le.fit_transform(processed_df.Condition2)
+    processed_df.BldgType= le.fit_transform(processed_df.BldgType)
+    processed_df.BldgType= le.fit_transform(processed_df.BldgType)
+    processed_df.HouseStyle= le.fit_transform(processed_df.HouseStyle)
+    processed_df.RoofStyle= le.fit_transform(processed_df.RoofStyle)
+    processed_df.RoofMatl= le.fit_transform(processed_df.RoofMatl)
+    processed_df.Exterior1st= le.fit_transform(processed_df.Exterior1st)
+    processed_df.Exterior2nd= le.fit_transform(processed_df.Exterior2nd)
+    processed_df.MasVnrType = [str(x) for x in processed_df.MasVnrType]
+    processed_df.MasVnrType = le.fit_transform(processed_df.MasVnrType)
+    processed_df.ExterQual= le.fit_transform(processed_df.ExterQual)
+    processed_df.ExterCond= le.fit_transform(processed_df.ExterCond)
+    processed_df.Foundation= le.fit_transform(processed_df.Foundation)
+    processed_df.BsmtQual = [str(x) for x in processed_df.BsmtQual]
+    processed_df.BsmtQual = le.fit_transform(processed_df.BsmtQual)
+    processed_df.BsmtCond = [str(x) for x in processed_df.BsmtCond]
+    processed_df.BsmtCond = le.fit_transform(processed_df.BsmtCond)
+    processed_df.BsmtExposure = [str(x) for x in processed_df.BsmtExposure]
+    processed_df.BsmtExposure = le.fit_transform(processed_df.BsmtExposure)
+    processed_df.BsmtFinType1 = [str(x) for x in processed_df.BsmtFinType1]
+    processed_df.BsmtFinType1 = le.fit_transform(processed_df.BsmtFinType1)
+    processed_df.BsmtFinType2 = [str(x) for x in processed_df.BsmtFinType2]
+    processed_df.BsmtFinType2 = le.fit_transform(processed_df.BsmtFinType2)
+    processed_df.PoolQC = [str(x) for x in processed_df.PoolQC]
+    processed_df.PoolQC = le.fit_transform(processed_df.PoolQC)
+    processed_df.Fence = [str(x) for x in processed_df.Fence]
+    processed_df.Fence = le.fit_transform(processed_df.Fence)
+    processed_df.MiscFeature = [str(x) for x in processed_df.MiscFeature]
+    processed_df.MiscFeature = le.fit_transform(processed_df.MiscFeature)
+    processed_df.SaleType= le.fit_transform(processed_df.SaleType)
+    processed_df.SaleCondition= le.fit_transform(processed_df.SaleCondition)
+    #processed_df = processed_df.drop(['name'], axis=1)
+    #drop these categories
+#    processed_df = processed_df.drop(['home.dest'],axis=1)
+    return processed_df
+
+processed_df = preprocess_house_df(house_train_df)
+
+
+
+#Univariate Selection
+#
+#
+#Feature Extraction with Univariate Statistical Tests (Chi-squared for classification)
+X = processed_df.values[:,0:37]
+Y = processed_df.values[:,37]
+# feature extraction
+test = SelectKBest(score_func=chi2, k=4)
+fit = test.fit(X,Y)
+#summarize scores
+numpy.set_printoptions(precision=3)
+print(fit.scores_)
+features = fit.transform(X)
+# summareize selected features
+print(features[0:5,:])
 
 # do 2-sample t-test on the data to determine which is significant.
 # We will do this for each variable. 
@@ -76,61 +149,61 @@ plt.show()
 
 # pass this hm="how many data points", variance, step=how far on step up
 # the y-value per point, correlation
-def create_dataset(hm, variance, step=2, correlation=False):
-    val=1
-    ys = []
-    for i in range(hm):
-        y = val + random.randrange(-variance,variance)
-        ys.append(y)
-        if correlation and correlation=='pos':
-            val+=step
-        elif correlation and correlation == 'neg':
-            val -= step
-    xs = [i for i in range(len(ys))]
-    return np.array(xs, dtype=np.float64), np.array(ys,dtype=np.float64)
+#def create_dataset(hm, variance, step=2, correlation=False):
+#    val=1
+#    ys = []
+#    for i in range(hm):
+#        y = val + random.randrange(-variance,variance)
+#        ys.append(y)
+#        if correlation and correlation=='pos':
+#            val+=step
+#        elif correlation and correlation == 'neg':
+#            val -= step
+#    xs = [i for i in range(len(ys))]
+#    return np.array(xs, dtype=np.float64), np.array(ys,dtype=np.float64)
+#
+#def best_fit_slope_and_intercept(xs,ys):
+#    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / 
+#         ((mean(xs)**2)-mean(xs**2)))
+#    b= mean(ys) - m*mean(xs)
+#    return m, b
+#
+#def squared_error(ys_orig, ys_line):
+#    return sum((ys_line-ys_orig)**2)
+#
+#def coefficient_of_determination(ys_orig,ys_line):
+#    y_mean_line = [mean(ys_orig) for y in ys_orig]
+#    squared_error_reg = squared_error(ys_orig, ys_line)
+#    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+#    return 1 -(squared_error_reg/squared_error_y_mean)
+#    
+#def reg_line(xdata,ydata):
+#    #create best fit line
+#    xs = np.array(xdata)
+#    ys = np.array(ydata)
+#    regression_line = [(m*x)+b for x in xs]
+#    return regression_line
+#
+#def reg_assumptions():
+#    # Test assumptions for regression analysis.
+#    # First test that the scatterplot is close to linear. 
+#    
+#    # Next, test that the residual plot shows a random pattern. The residual 
+#    # value is the observed value minus the predicted value.
+#    return
+#    
+##xs, ys = create_dataset(40, 80, 2, correlation='pos')
+#xs, ys = create_dataset(40, 80, 2, correlation=False)
+#
+#m,b = best_fit_slope_and_intercept(xs,ys)
+#regression_line = [(m*x)+b for x in xs]
+#predict_x = 8
+#predict_y = (m*predict_x)+b
+#
+#r_squared = coefficient_of_determination(ys, regression_line)
+#print(r_squared)
 
-def best_fit_slope_and_intercept(xs,ys):
-    m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / 
-         ((mean(xs)**2)-mean(xs**2)))
-    b= mean(ys) - m*mean(xs)
-    return m, b
-
-def squared_error(ys_orig, ys_line):
-    return sum((ys_line-ys_orig)**2)
-
-def coefficient_of_determination(ys_orig,ys_line):
-    y_mean_line = [mean(ys_orig) for y in ys_orig]
-    squared_error_reg = squared_error(ys_orig, ys_line)
-    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
-    return 1 -(squared_error_reg/squared_error_y_mean)
-    
-def reg_line(xdata,ydata):
-    #create best fit line
-    xs = np.array(xdata)
-    ys = np.array(ydata)
-    regression_line = [(m*x)+b for x in xs]
-    return regression_line
-
-def reg_assumptions():
-    # Test assumptions for regression analysis.
-    # First test that the scatterplot is close to linear. 
-    
-    # Next, test that the residual plot shows a random pattern. The residual 
-    # value is the observed value minus the predicted value.
-    return
-    
-#xs, ys = create_dataset(40, 80, 2, correlation='pos')
-xs, ys = create_dataset(40, 80, 2, correlation=False)
-
-m,b = best_fit_slope_and_intercept(xs,ys)
-regression_line = [(m*x)+b for x in xs]
-predict_x = 8
-predict_y = (m*predict_x)+b
-
-r_squared = coefficient_of_determination(ys, regression_line)
-print(r_squared)
-
-plt.scatter(xs,ys)
-plt.scatter(predict_x,predict_y,s=100, color='g')
-plt.plot(xs,regression_line)
-plt.show()
+#plt.scatter(xs,ys)
+#plt.scatter(predict_x,predict_y,s=100, color='g')
+#plt.plot(xs,regression_line)
+#plt.show()
