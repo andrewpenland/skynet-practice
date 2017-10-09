932fde347e17bafbb701205fb13611e0d2fb1d85
cecilartavion <30665264+cecilartavion@users.noreply.github.com>
2017-10-09 16:31:15 -0400

    Worked on digits
    Identified digits using genetic programming.

--- genetic_algorithm/neural_ga.py
+++ genetic_algorithm/neural_ga.py
@@ -1,23 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  9 13:12:42 2017
+
+@author: jasplund
+"""
+
 from deap import base,creator,tools,gp, algorithms
 import random
 import operator
 import math
 import numpy
+import pandas as pd
 
-#create protected division so that we don't crash program 
-#by dividing by zero
+from sklearn import datasets, svm, cross_validation, tree, preprocessing, metrics
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import chi2
+from sklearn.neural_network import MLPClassifier
+import sklearn.ensemble as ske
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 
+digits_train_df = pd.read_csv('optdigits.tra',header=None,prefix='x')
+
+digits_test_df = pd.read_csv('optdigits.tes', header=None, prefix='x')
+print(str(digits_test_df.head()))
+
+train_array = digits_train_df.values
+X_train = train_array[:,0:64]
+y_train = train_array[:,64]
+
+print(len(X_train))
+
+test_array = digits_test_df.values
+X_test = test_array[:,0:64]
+y_test = test_array[:,64]
+
+print(str(y_train), ' ', len(y_train))
+
+def log_func(x):
+    return 10.0/(1.0+math.exp(-x))
+
+def fitness_opt(individual):
+    func = toolbox.compile(expr=individual)
+    total = 0
+#    print(y_train[0])
+#    print(str(individual), ' ',len(y_train))
+    for i in range(len(y_train)):
+        try:
+            xt = X_train[i]
+            total += (log_func(func(*xt))-y_train[i])**2
+        except OverflowError:
+            total = 1000000
+    return total,
+    
 def protectedDiv(left,right):
-   try:
+   if right==0:
+       return 1
+   else:
       return left/right
-   except ZeroDivisionError:
-      return 1
 
 #add a basic set of operators
 #the "1" in the second argument of PrimitiveSet
 #is the number of inputs.
 
-pset = gp.PrimitiveSet("MAIN", 1)
+pset = gp.PrimitiveSet("MAIN", 64)
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.sub,2)
 pset.addPrimitive(operator.mul,2)
@@ -25,7 +71,7 @@ pset.addPrimitive(protectedDiv,2)
 pset.addPrimitive(operator.neg, 1)
 pset.addPrimitive(math.cos,1)
 pset.addPrimitive(math.sin,1)
-pset.addPrimitive(math.exp,1)
+#pset.addPrimitive(math.exp,1)
 #pset.addPrimitive(math.sqrt,1)
 #pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))	
 
@@ -38,11 +84,8 @@ def plog(x):
     else:
         return 0
         
-pset.addPrimitive(plog,1)
+#pset.addPrimitive(plog,1)
 
-#Rename the variables
-
-pset.renameArguments(ARG0='x')
 
 #We need an individual with a genotype
 
@@ -57,40 +100,14 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ex
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-#create a function that evaluates the individual at a number of points
-#(essentially determines the fitness function. Notice that it returns
-#a tuple (which I think is why there is a "," on the end), since fitness
-#must be an iterable
-
-#in addition to the mean root square error, I am going to
-#penalize the function for each time that it returns 0
-
-def evalSymbReg(individual,points):
-   #Transform the tree expression into a callable function
-   func = toolbox.compile(expr=individual)
-   #Evaluate the mean squared error between the expression
-   #and the real function: x**4 +  x**3 +  x**2 + x
-   sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
-   numzeros_bad_errors = 0
-   for x in points:
-       if math.fabs(func(x) - x**4 - x**3 - x**2 - x) >0.1:
-           numzeros_bad_errors += 1000000
-   #print(sqerrors)
-   return (math.fsum(sqerrors)/len(points) + numzeros_bad_errors),
-
-#this evaluates our symbolic expression at evenly spaced points in [-1,1]
-#could just as easily make this be on a randomly generated set of points 
-   
-random.seed(318)
-
-toolbox.register("evaluate", evalSymbReg, points = [x/10.0 for x in range(-10,10)])
+toolbox.register("evaluate", fitness_opt)
 toolbox.register("select", tools.selTournament, tournsize = 3)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_ = 0, max_ = 2)
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
-toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 17))
-toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 17))
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 30))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value = 30))
 
 #now we set up statistics 
 
@@ -105,26 +122,16 @@ mstats.register("max",numpy.max)
 # number of candidate solutions
 pop = toolbox.population(n=300)
 hof = tools.HallOfFame(10)
-pop, log = algorithms.eaSimple(pop, toolbox, 0.5,0.1,40, stats=mstats, halloffame=hof,verbose=True)
+#pop, log = algorithms.eaSimple(pop, toolbox, 0.5,0.1,40, stats=mstats, halloffame=hof,verbose=True)
 
 #print(hof)
 for x in hof:
     print(str(x) + '\n')
+    
+def miracle_func(t):
+    x = X_train[t]
+    return -(protectedDiv(x[63],(x[29]*x[56]-(math.sin(x[16])-(x[29]*x[63]*x[56]-(x[39]-x[28]))))))
 
-#expr = hof[0]
-#nodes, edges, labels = gp.graph(expr)
-#
-#### Graphviz Section ###
-#import pygraphviz as pgv
-#
-#g = pgv.AGraph()
-#g.add_nodes_from(nodes)
-#g.add_edges_from(edges)
-#g.layout(prog="dot")
-#
-#for i in nodes:
-#    n = g.get_node(i)
-#    n.attr["label"] = labels[i]
-#
-#g.draw("tree.pdf")
+print(sum([math.floor(log_func(miracle_func(t)))==y_train[t] for t in range(len(y_train))])/len(y_train))
 
+    
